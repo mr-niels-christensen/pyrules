a6a0da58ccbfb39a26091c648e5d815f3acba6f2
Niels H. Christensen <nhc@mayacs.com>
2015-09-09 12:20:54 +0100

    Documentation

--- src/pyrules2/evaluation.py
+++ src/pyrules2/evaluation.py
@@ -9,6 +9,19 @@ class RuleBook(object):
 
 
 class _ParseTree(object):
+    """
+    A _ParseTree represents a program defined using the
+    public decorators and functions.
+    """
+
+    """
+    These are the node types in a parse tree:
+    MATCHES: just match the terms to tuples from an iterator
+    AND: get results from child trees and return the compatible ones,
+         i.e. the ones where a variable has compatible values.
+    OR: get results from child trees and return the union of these.
+    LIMIT: get the initial results from the child tree.
+    """
     MATCHES, AND, OR, LIMIT = range(4)
     NODE_TYPES = set([MATCHES, AND, OR, LIMIT])
 
@@ -27,6 +40,10 @@ class _ParseTree(object):
         return _ParseTree(_ParseTree.OR, self, other)
 
     def to_dict_iterator(self):
+        """
+        This is the "code generation" method.
+        :return: An iterator of dicts. Each dict maps variables to concrete values.
+        """
         sub_iterators = [s.to_dict_iterator() for s in self._sub_trees]
         if self._node_type == _ParseTree.MATCHES:
             return _dict_iterator_for_matches(self._parameters['tuple_iterator'],
@@ -42,6 +59,15 @@ class _ParseTree(object):
 
 
 def limit(max_results):
+    """
+    Example usage:
+    @limit(1)
+    def p(self, x):
+        return matches([atom.A, atom.B], x) # In fact, only x==atom.A will be a solution because of @limit
+    :param max_results: The maximum number of results to return, e.g. 200
+    :return: A method returning an initial slice of the results provided
+    by the decorated method.
+    """
     def _limit(method):
         def _resulting_method(self, *args, **kwargs):
             return _ParseTree(
@@ -53,6 +79,14 @@ def limit(max_results):
 
 
 def matches(tuple_iterator, *args):
+    """
+    Example usage:
+    def p(self, x):
+        return matches([atom.A, atom.B], x)
+    :param tuple_iterator: An iterator of tuples, e.g. [(atom.A, atom.B), (atom.C, atom.D)]
+    :param args: A tuple of terms. Each tuple from tuple_iterator must be the same size as args.
+    :return: A program that matches the given args every tuple in the iterator.
+    """
     # TODO consider better syntactical sugar, e.g. find(x,y).in([...])
     # or something corresponding to reduce(|,match(x,y,foo) for foo in [...])
     # or @rule(style=tuples) def p(x,y): return [...]
@@ -75,6 +109,14 @@ def _dict_iterator_for_matches(tuple_iterator, args):
 
 
 def rule(func):
+    """
+    Example usage:
+    @rule # Call p like this: for y in r.p(atom.A, var.y): print y
+    def p(self, x, y):
+        return matches([(atom.A, atom.B), (atom.C, atom.D)], x, y)
+    :param func: A method returning a program.
+    :return: Solutions
+    """
     def resulting_method(self, *args):
         assert all(is_term(arg) for arg in args)
         parse_tree = func(self, *args)
@@ -83,7 +125,7 @@ def rule(func):
         def projection(d):
             return tuple(d[arg] for arg in args if arg.is_var())
         return (projection(d) for d in dict_iterator)
-    return resulting_method
+    return wraps(func)(resulting_method)
 
 
 def _union(ds):
