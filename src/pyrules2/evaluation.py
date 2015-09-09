695c78b47077a49e06ab40e0a72d8e77e999f58f
Niels H. Christensen <nhc@mayacs.com>
2015-09-09 10:46:57 +0100

    PEP8 fixes

--- src/pyrules2/evaluation.py
+++ src/pyrules2/evaluation.py
@@ -3,8 +3,10 @@ from collections import deque
 from prolog_like_terms import is_term
 from functools import wraps
 
+
 class RuleBook(object):
     pass
+
 
 def limit(max_results):
     def _limit(method):
@@ -13,12 +15,13 @@ def limit(max_results):
         return wraps(method)(_resulting_method)
     return _limit
 
+
 def matches(tuple_iterator, *args):
-    #TODO consider better syntactical sugar, e.g. find(x,y).in([...])
-    #or something corresponding to reduce(|,match(x,y,foo) for foo in [...])
-    #or @rule(style=tuples) def p(x,y): return [...]
+    # TODO consider better syntactical sugar, e.g. find(x,y).in([...])
+    # or something corresponding to reduce(|,match(x,y,foo) for foo in [...])
+    # or @rule(style=tuples) def p(x,y): return [...]
     var_indexes = dict()
-    #TODO handle when same var appears multipe times
+    # TODO handle when same var appears multipe times
     for index, arg in enumerate(args):
         if arg.is_var():
             var_indexes[arg] = index
@@ -27,11 +30,13 @@ def matches(tuple_iterator, *args):
     map = lambda t : {v : t[var_indexes[v]] for v in var_indexes}
     return _Wrap(map(t) for t in tuple_iterator)
 
+
 def rule(func):
     def resulting_method(self, *args):
         assert all(is_term(arg) for arg in args)
         return func(self, *args).set_args(*args)
     return resulting_method
+
 
 class _Wrap(object):
     def __init__(self, wrapped):
@@ -50,7 +55,7 @@ class _Wrap(object):
         return self._projection(n)
 
     def __and__(self, other):
-        #TODO: propagate bindings!
+        # TODO: propagate bindings!
         assert isinstance(other, _Wrap)
         prod = product(self._wrapped, other._wrapped)
         return _Wrap(imap(_union, ifilter(_compatible, prod)))
@@ -59,11 +64,13 @@ class _Wrap(object):
         assert isinstance(other, _Wrap)
         return _Wrap(_roundrobin(self._wrapped, other._wrapped))
 
+
 def _union(ds):
     d0, d1 = ds
     d = d0.copy()
     d.update(d1)
     return d
+
 
 def _compatible(ds):
     d0, d1 = ds
@@ -72,10 +79,11 @@ def _compatible(ds):
             return False
     return True
 
+
 def _roundrobin(*iterables):
-    '''Splices the given iterables fairly, see
+    """Splices the given iterables fairly, see
        http://bugs.python.org/issue1757395
-    '''
+    """
     pending = deque(iter(i).next for i in reversed(iterables))
     rotate, pop, _StopIteration = pending.rotate, pending.pop, StopIteration
     while pending:
