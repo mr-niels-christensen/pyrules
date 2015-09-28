930ea630ced63e95b179bb360b0e9d20c8020a19
Niels H. Christensen <nhc@mayacs.com>
2015-09-28 14:07:24 +0100

    AndExpression OK

--- src/test/test_expr.py
+++ src/test/test_expr.py
@@ -44,16 +44,79 @@ class Test(unittest.TestCase):
         self.assertIn({'b': 1}, r)
         self.assertEqual(2, len(r))
         # >2 subexpressions
+        s = [ConstantExpression({letter: index}) for index, letter in enumerate('abcdef')]
+        o = OrExpression(*s)
+        r = list(o.all_dicts())
+        for index, letter in enumerate('abcdef'):
+            self.assertIn({letter: index}, r)
+        self.assertEqual(6, len(r))
         # Same dict twice
+        o = OrExpression(ConstantExpression({'a': 0}), ConstantExpression({'a': 0}))
+        self.assertEqual([{'a': 0}, {'a': 0}], list(o.all_dicts()))
         # dicts with same key but different value
+        o = OrExpression(ConstantExpression({'a': 0}),
+                         ConstantExpression({'a': 1}))
+        r = list(o.all_dicts())
+        self.assertIn({'a': 0}, r)
+        self.assertIn({'a': 1}, r)
+        self.assertEqual(2, len(r))
         # OR subexpression
+        o = OrExpression(OrExpression(), OrExpression(ConstantExpression(dict())))
+        self.assertEqual([dict()], list(o.all_dicts()))
         # Argument is not an Expression
+        self.assertRaises(Exception, OrExpression, 42)
+        self.assertRaises(Exception, OrExpression, {'a': 'b'})
         # Repeated invocations
-
-    def test_or_op(self):
-        pass  # TODO
+        o = OrExpression(ConstantExpression({'a': 'b'}))
+        self.assertListEqual([{'a': 'b'}], list(o.all_dicts()))
+        self.assertListEqual([{'a': 'b'}], list(o.all_dicts()))
 
     def test_and(self):
+        # 0 subexpressions
+        o = AndExpression()
+        self.assertListEqual([], list(o.all_dicts()))
+        # 1 subexpression
+        o = AndExpression(ConstantExpression({'a': 'b'}))
+        self.assertListEqual([{'a': 'b'}], list(o.all_dicts()))
+        # 2 subexpressions, independent
+        o = AndExpression(ConstantExpression({'a': 0}),
+                          ConstantExpression({'b': 1}))
+        self.assertListEqual([{'a': 0, 'b': 1}], list(o.all_dicts()))
+        # >2 subexpressions, independent
+        s = [ConstantExpression({letter: index}) for index, letter in enumerate('abcdef')]
+        o = AndExpression(*s)
+        r = list(o.all_dicts())
+        self.assertEqual(1, len(r))
+        d = r[0]
+        for index, letter in enumerate('abcdef'):
+            self.assertEquals(index, d[letter])
+        self.assertEqual(6, len(d))
+        # Same dict twice
+        o = AndExpression(ConstantExpression({'a': 0}), ConstantExpression({'a': 0}))
+        self.assertEqual([{'a': 0}], list(o.all_dicts()))
+        # dicts with same key but different value
+        o = AndExpression(ConstantExpression({'a': 0, 'b': 1}),
+                          ConstantExpression({'a': 1, 'b': 1}))
+        self.assertListEqual([], list(o.all_dicts()))
+        # AND subexpression, 1 empty
+        o = AndExpression(AndExpression(), AndExpression(ConstantExpression(dict())))
+        self.assertEqual([], list(o.all_dicts()))
+        # AND subexpression
+        o = AndExpression(AndExpression(ConstantExpression(dict())),
+                          AndExpression(ConstantExpression(dict())))
+        self.assertEqual([dict()], list(o.all_dicts()))
+        # Argument is not an Expression
+        self.assertRaises(Exception, AndExpression, 42)
+        self.assertRaises(Exception, AndExpression, {'a': 'b'})
+        # Repeated invocations
+        o = AndExpression(ConstantExpression({'a': 'b'}))
+        self.assertListEqual([{'a': 'b'}], list(o.all_dicts()))
+        self.assertListEqual([{'a': 'b'}], list(o.all_dicts()))
+
+    def test_when(self):
+        pass  # TODO
+
+    def test_or_op(self):
         pass  # TODO
 
     def test_and_op(self):
@@ -62,8 +125,9 @@ class Test(unittest.TestCase):
     def test_and_or(self):
         pass  # TODO
 
-    def test_when(self):
+    def test_dag(self):
         pass  # TODO
+
 
 
 
