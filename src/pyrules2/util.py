e6f602c9ac6d351b958c1f22663d034eb1075f2a
Niels H. Christensen <nhc@mayacs.com>
2015-10-20 22:46:04 +0100

    Improve documentation of lazy_product()

--- src/pyrules2/util.py
+++ src/pyrules2/util.py
@@ -19,21 +19,54 @@ def round_robin(*iterables):
             pop()
 
 
-def lazy_product(*generators):
-    tuple_size = len(generators)
-    if tuple_size == 0:
+def _enumerated_fair_iterator(iterators):
+    """
+    Combines iterators into one. Example: with input [['a','b'], ['x','y']]
+    the returned iterator is equivalent to
+    [(1, (0, 'a')), (2, (1, 'x')), (3, (0, 'b')), (4, (1, 'y'))]
+    Each original value is paired with the index of the iterator it came from.
+    Each pair is annotated with a serial number independent which generator it came from.
+    The iterators are polled using round_robin above.
+    :param iterators: Any number of iterators, finite or infinite.
+    :return: The combined iterator.
+    """
+    indexed_iterators = [izip(repeat(index), g) for index, g in enumerate(iterators)]
+    fair_iterators = round_robin(*indexed_iterators)
+    enumerated_fair_iterators = izip(count(1), fair_iterators)
+    return enumerated_fair_iterators
+
+
+def lazy_product(*iterators):
+    """
+    Akin to itertools.product except
+        - works with infinite iterators
+        - does not order the generated tuples in the same way
+    For finite iterators, the following should hold:
+        set(itertools.product(*iterators)) == set(lazy_product(*iterators))
+    :param iterators: Any number of iterators
+    :return: One generator that yields tuples. Each tuple contains
+    one value from each of the input iterators.
+    """
+    tuple_size = len(iterators)
+    if tuple_size == 0:  # Special case: Just yield the empty tuple
         yield ()
         return
-    indexed_generators = [izip(repeat(index), g) for index, g in enumerate(generators)]
-    fair_generator = round_robin(*indexed_generators)
-    enumerated_fair_generator = izip(count(1), fair_generator)
-    cache = [list() for _ in range(tuple_size)]  # TODO sets of frozendicts
-    for counter, (index, value) in enumerated_fair_generator:
+    # General case: Keep a cache of every seen value, per iterator
+    cache = [list() for _ in range(tuple_size)]
+    #   TODO The above lists should be sets.
+    #   To make our dicts hashable, see
+    #   http://stackoverflow.com/questions/9997176/immutable-dictionary-only-use-as-a-key-for-another-dictionary
+    # For each new value from one of the iterators...
+    for counter, (index, value) in _enumerated_fair_iterator(iterators):
+        # Add it to the cache
         cache[index].append(value)
+        # If every iterator has been polled once and one was empty: Return
         if counter == tuple_size:
             if any([len(l) == 0 for l in cache]):
                 return  # Some generator did not deliver a value
+        # If every iterator has provided at least one value:
         if counter >= tuple_size:  # TODO: Only if value not seen before
+            # Generate every tuple possible using the new value
             x = list(cache)
             x[index] = [value]
             for t in product(*x):
