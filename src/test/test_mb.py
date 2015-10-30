1ec763c551a3badabea2324c29d6975f3d215571
Niels H. Christensen <nhc@mayacs.com>
2015-10-30 14:51:39 +0000

    Sketch of monkey-banana example

---
+++ src/test/test_mb.py
@@ -0,0 +1,96 @@
+import unittest
+from pyrules2 import when, rule, RuleBook
+from pyrules2.rules import Var
+from pyrules2.expression import RenameExpression
+from itertools import product
+from collections import namedtuple
+
+'''This pyrules example goes out to the Prolog enthusiasts.
+   It is an implementation of Ivan Bratko's monkey&banana example:
+   http://books.google.co.uk/books?id=-15su78YRj8C&pg=PA46&lpg=PA46&dq=monkey+banana+bratko&source=bl&ots=TA22Ve7wHK&sig=a1x6v2nvu7C78wjAs2lGMOKXUt4&hl=en&sa=X&ei=buKdUqioJeWX7QaLloCoAg&ved=0CC8Q6AEwAA#v=onepage&q=monkey%20banana%20bratko&f=false
+
+   Our protagonist monkey is in a room with a door and a window. In the middle of the room,
+   a banana is hanging from a rope in the ceiling. The monkey wants the banana, but it can't reach that high.
+'''
+POSITIONS = ['middle', 'atdoor', 'atwindow']
+'''
+   Luckily, there's a box at the window that the monkey can push around and climb.
+'''
+LEVELS = ['onfloor', 'onbox']
+'''
+   The monkey does not have the banana, but if it climbs the box in the middle of the room
+   and grasps the banana, then it will have its banana.
+'''
+HAS_HASNOT = ['has', 'hasnot']
+'''
+   Bratko's model of this puzzle takes the form
+       (state, move, state2)
+   where each state is on the form
+       ('state',  monkey_pos, monkey_level, box_pos, has_hasnot)
+   and a move is 'grasp', 'climb', ('walk', pos1, pos2) or ('push', pos1, pos2).
+
+    TODO
+   I.e. starting from the door, our monkey can get its banana.
+'''
+
+State = namedtuple('State', ['monkey_pos', 'monkey_level', 'box_pos', 'has'])
+
+
+def climb(state):
+    if state.monkey_level == 'onfloor' and state.monkey_pos == state.box_pos:
+        yield State(state.monkey_pos, 'onbox', state.monkey_pos, state.has)
+
+
+def grasp(state):
+    if state == State('middle', 'onbox', 'middle', False):
+        yield State('middle', 'onbox', 'middle', True)
+
+
+def push(state):
+    if state.monkey_level == 'onfloor' and state.monkey_pos == state.box_pos:
+        for new_pos in POSITIONS:
+            if not new_pos == state.monkey_pos:
+                yield State(new_pos, 'onfloor', new_pos, state.has)
+
+
+def walk(state):
+    if state.monkey_level == 'onfloor':
+        for new_pos in POSITIONS:
+            if not new_pos == state.monkey_pos:
+                yield State(new_pos, 'onfloor', state.box_pos, state.has)
+
+def expand(fs, **kwargs):
+    raise NotImplementedError('TODO')
+
+
+class MonkeyBanana(RuleBook):
+    #canget(Statel) :- move(Statel, Move, State2), canget(State2).
+    #canget(state(-, -, -, has)).
+    #move(state( middle, onbox, middle, hasnot), grasp, state( middle, onbox, middle, has)).
+    #move(state(P, onfloor, P, H), climb, state(P, onbox, P, H)).
+    #move(state(Pl, onfloor, Pl, H), push(Pl, P2), state(P2, onfloor, P2, H)).
+    #move(state(Pl, onfloor, B, H), walk(Pl, P2), state(P2, onfloor, B, H)).
+
+    @rule
+    def can_get(self, state, via_state=None):
+        #TODO: Don't require 'middle' and 'onbox'
+        return when(state=State('middle', 'onbox', 'middle', True))\
+               & self.can_go(state)
+
+    @rule
+    def can_go(self, state):
+        return expand([climb, walk, push, grasp], state=State('atdoor', 'onfloor', 'atwindow', False))
+
+
+class Test(unittest.TestCase):
+    def test_cls(self):
+        print MonkeyBanana
+
+    def test_types(self):
+        print list(MonkeyBanana().pos(None).all_dicts())
+        print list(MonkeyBanana().level(None).all_dicts())
+        print list(MonkeyBanana().bool(None).all_dicts())
+
+
+if __name__ == "__main__":
+    unittest.main()
